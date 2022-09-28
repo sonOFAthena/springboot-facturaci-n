81897a914dea3c37c76834558feec4a59c69c02a
josexrg <josexrg@hotmail.com>
2022-09-28 17:52:12 -0500

    ajax facturas implemented

--- src/main/java/com/artion/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/artion/springboot/app/models/service/ClienteServiceImpl.java
@@ -1,7 +1,9 @@
 package com.artion.springboot.app.models.service;
 
 import com.artion.springboot.app.models.dao.IClienteDao;
+import com.artion.springboot.app.models.dao.IProductoDao;
 import com.artion.springboot.app.models.entity.Cliente;
+import com.artion.springboot.app.models.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,9 @@ public class ClienteServiceImpl implements IClienteService{
 
     @Autowired
     private IClienteDao clienteDao;
+
+    @Autowired
+    private IProductoDao productoDao;
 
     @Override
     @Transactional(readOnly = true)
@@ -44,5 +49,11 @@ public class ClienteServiceImpl implements IClienteService{
     @Transactional()
     public void delete(Long id) {
         clienteDao.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByNombre(String term) {
+        return productoDao.findByNombreLikeIgnoreCase("%" + term + "%");
     }
 }
--- src/main/java/com/artion/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/artion/springboot/app/models/service/IClienteService.java
@@ -1,8 +1,10 @@
 package com.artion.springboot.app.models.service;
 
 import com.artion.springboot.app.models.entity.Cliente;
+import com.artion.springboot.app.models.entity.Producto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +14,5 @@ public interface IClienteService {
     public void save(Cliente cliente);
     public Cliente findOne(Long id);
     public void delete(Long id);
+    public List<Producto> findByNombre(String term);
 }
